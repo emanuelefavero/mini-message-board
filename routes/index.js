5e03046f1f616b1bd8f3f30f3acf0dee18f2ce3f
emanuelefavero <emafaux89@gmail.com>
2022-10-17 11:56:24 +0200

    add messages and create new message page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+// Messages Data Array of Objects
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World',
+    user: 'Charles',
+    added: new Date(),
+  },
+]
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Mini Message Board', messages })
+})
 
-module.exports = router;
+// New message form
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message' })
+})
+
+router.post('/new', (req, res, next) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date(),
+  })
+  res.redirect('/')
+})
+
+module.exports = router
